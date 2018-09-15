a37d298200ecf73e7c2ee05b86c6569bdb2676fa
lrbrwnn1 <lrbrwnn1@users.noreply.github.com>
2018-09-14 21:03:32 -0500

    Update and rename Rectangle Volume to RectangleVolume.py

--- RectangleVolume.py
+++ RectangleVolume.py
@@ -1,5 +1,7 @@
 import math
 
+#for calculating volume of water in a rectangular aquarium
+
 Length = float(input("Please enter the length of the container in inches"))
 Width = float(input("Please enter the width of the container in inches"))
 Height = float(input("Please enter the height of the container in inches"))
