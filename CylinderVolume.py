1231aeb35a8133c8a729613442b120a09db1fe8c
lrbrwnn1 <lrbrwnn1@users.noreply.github.com>
2018-09-14 21:04:06 -0500

    Update and rename Cylinder Volume to CylinderVolume.py

--- CylinderVolume.py
+++ CylinderVolume.py
@@ -1,5 +1,7 @@
 import math
 
+#for calculating volume of water in a cylindrical aquarium
+
 Radius = float(input("Please enter the radius of the container in inches"))
 Height = float(input("Please enter the height of the container in inches"))
 Volume = ((3.1415*(Radius*Radius)*Height)*0.004329)
